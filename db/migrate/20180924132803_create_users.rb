7243a07a9dbed87dd810709741e8f211e71b7b79
Ofer Schonberger <ofer.schonberger@wework.com>
2018-09-24 20:14:46 +0300

    Added posts. for some reason this makes Users work. meh.

--- db/migrate/20180924132803_create_users.rb
+++ db/migrate/20180924132803_create_users.rb
@@ -1,8 +1,12 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
+class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password
+      t.string :title
+      t.string :cuisine
+      t.decimal :rating
+      t.boolean :acceptes_10bis
+      t.text :address
+      t.decimal :max_delivery_time
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_132803) do
+ActiveRecord::Schema.define(version: 2018_09_24_171139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
+    t.string "title"
+    t.string "cuisine"
+    t.decimal "rating"
+    t.boolean "acceptes_10bis"
+    t.text "address"
+    t.decimal "max_delivery_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
